b05363a8e7eb0c1c75dfa991ca21625de6fadb3b
FilippoGorini <filippogorini7@gmail.com>
2026-02-10 16:41:04 +0100

    EX0: slight refinements

--- Assignment 0/main.m
+++ Assignment 0/main.m
@@ -30,17 +30,18 @@ frequencies = linspace(-fs1/2, fs1/2, n_samples);
 subplot(2,1,2)
 plot(frequencies, fftshift(abs(fourier)));
 xlabel("Frequency (Hz)");
-ylabel("|Amplitude|");
+ylabel("FFT Magnitude");
 title("Data1 - Frequency Spectrum (FFT)");
 grid on;
 
 % --- Interpretation ---
-% The signal shows very fast and irregular fluctuations, with bursts of 
+% The time domain signal shows very fast and irregular fluctuations, with bursts of 
 % high amplitude activity separated by quieter periods.
-% This behavior is typical of EMG (electromyography) data, which captures
+% This behavior is typical of raw EMG (electromyography) data, which captures
 % muscle activity.
 % EMG signals usually contain high-frequency components about 20–500 Hz,
-% this can also be observed by the frequency spectrum of the analyzed signal.
+% this can also be observed by the frequency spectrum of the analyzed signal
+% (in particular in our case we have a peak at about 35 Hz, with smaller components all the way up to 400/500 Hz).
 % The high sampling rate (2000 Hz) is consistent with this signal type.
 
 
@@ -69,16 +70,16 @@ hold on;
 plot(frequencies, fftshift(abs(fourier2)));
 hold off;
 xlabel("Frequency (Hz)");
-ylabel("|Amplitude|");
+ylabel("FFT Magnitude");
 title("Data2 - Frequency Spectrum (FFT)");
 grid on;
 legend("Channel 1", "Channel 2");
 
 % --- Interpretation ---
-% The signal displays a series of isolated periodic spikes that appear every few seconds.
-% This pattern is typical of motion data, where spikes represent discrete movements (e.g., steps).
+% The signal displays a series of isolated periodic spikes that appear every few seconds on both channels.
+% This pattern is typical of motion data, where spikes represent discrete movements.
 % The low sampling rate (166 Hz) supports this interpretation.
-% Energy is concentrated in low frequencies, between around -6 Hz and 6 Hz, consistent with motion data.
+% Energy is concentrated in low frequencies, below 6 Hz, which is also consistent with motion data.
 
 
 %% ---------------- Third dataset ----------------
@@ -95,21 +96,32 @@ title("Data3 - Time domain");
 grid on;
 
 % ---- Frequency domain (FFT) ----
-n_samples = numel(x3);
-fourier = fft(x3);
+
+% This signal is not zero mean, therefore we center it to avoid having a
+% big spike at 0 Hz in the fft:
+x3_centered = x3 - mean(x3);
+
+n_samples = numel(x3_centered);
+fourier = fft(x3_centered);
 frequencies = linspace(-fs3/2, fs3/2, n_samples);
 subplot(2,1,2)
 plot(frequencies, fftshift(abs(fourier)));
 xlabel("Frequency (Hz)");
-ylabel("|Amplitude|");
+ylabel("FFT Magnitude");
+
+% x and y axis limits to better show the relevant frequencies
+ylim([0, 15000]); 
+xlim([-25, 25]);
+
 title("Data3 - Frequency Spectrum (FFT)");
 grid on;
 
 % --- Interpretation ---
-% The signal shows slow, continuous oscillations without sharp spikes.
-% The amplitude changes gradually.
+% The signal shows slower continuous oscillations with some noise.
+% The signal was not zero mean so the offset was removed before doing the
+% fft.
 % This behavior is characteristic of EEG (electroencephalography) data, 
 % representing brain activity.
-%  EEG signals generally contain low-frequency components, in this case
-% mostly between -0.01 Hz and 0.01 Hz.
-% The sampling frequency of 250 Hz is standard for EEG recordings.
+%  EEG signals generally contain low-frequency components, and in this case, 
+% even if not very pronounced, there are some components at about 6 and 10 Hz which is plausible for eeg signals.
+% The sampling frequency of 250 Hz also is good for EEG recordings.
